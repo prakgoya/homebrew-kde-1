27d649f7d55095aca001da649ecc65fb2f200149
Yurii Kolesnykov <yurikoles@gmail.com>
2018-04-14 20:02:11 +0300

    new formula: elisa 0.1

---
+++ Formula/elisa.rb
@@ -0,0 +1,44 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Elisa < Formula
+  desc "Elisa Music Player"
+  homepage ""
+  url "https://github.com/KDE/elisa/archive/v0.1.tar.gz"
+  sha256 "379561628498a1bb3fa4339f9a160239a96e889b28acaec8ca7f27d4b60fb62a"
+
+  head "https://github.com/KDE/elisa.git"
+  depends_on "cmake" => :build
+  depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
+  depends_on "KDE-mac/kde/kf5-kfilemetadata"
+  depends_on "qt5"
+  depends_on "gettext"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DKDE_INSTALL_QMLDIR=lib/qt5/qml"
+    args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+    args << "-DCMAKE_INSTALL_BUNDLEDIR=#{bin}"
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      prefix.install "install_manifest.txt"
+    end
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test elisa`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
