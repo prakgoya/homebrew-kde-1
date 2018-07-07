28309300f07bead84d605a04ddb4e5994406dc33
Yurii Kolesnykov <yurikoles@gmail.com>
2018-07-07 22:49:59 +0300

    fix cmake args

--- Formula/atelier.rb
+++ Formula/atelier.rb
@@ -17,6 +17,12 @@ class Atelier < Formula
   depends_on "qwt"
 
   def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
+    args << "-DKDE_INSTALL_QMLDIR=lib/qt5/qml"
+    args << "-DKDE_INSTALL_PLUGINDIR=lib/qt5/plugins"
+
     mkdir "build" do
       system "cmake", "..", *args
       system "make", "install"
