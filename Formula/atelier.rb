18115394ee69e26aa69c835ba7d2b0c9e026fd49
Yurii Kolesnykov <yurikoles@gmail.com>
2018-07-07 22:20:21 +0300

    fix audit issues

--- Formula/atelier.rb
+++ Formula/atelier.rb
@@ -7,17 +7,16 @@ class Atelier < Formula
   head "git://anongit.kde.org/atelier.git"
 
   depends_on "cmake" => :build
-  depends_on "qwt"
   depends_on "KDE-mac/kde/kf5-extra-cmake-modules" => :build
-  depends_on "KDE-mac/kde/kf5-ki18n" 
-  depends_on "KDE-mac/kde/kf5-configwidgets"
-  depends_on "KDE-mac/kde/kf5-kxmlgui"
-  depends_on "KDE-mac/kde/kf5-ktexteditor"
-  depends_on "KDE-mac/kde/kf5-knewstuff"
   depends_on "KDE-mac/kde/atcore"
+  depends_on "KDE-mac/kde/kf5-kconfigwidgets"
+  depends_on "KDE-mac/kde/kf5-ki18n"
+  depends_on "KDE-mac/kde/kf5-knewstuff"
+  depends_on "KDE-mac/kde/kf5-ktexteditor"
+  depends_on "KDE-mac/kde/kf5-kxmlgui"
+  depends_on "qwt"
 
   def install
-
     mkdir "build" do
       system "cmake", "..", *args
       system "make", "install"
